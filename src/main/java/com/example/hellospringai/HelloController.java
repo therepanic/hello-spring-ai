8efb7e61c17121c5fa4b83346f47c2415cd8da1c
Andrey Litvitski <andrey1010102008@gmail.com>
2025-07-22 21:43:43 +0300

    add model calling

---
+++ src/main/java/com/example/hellospringai/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.hellospringai;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final ChatClient chatClient;
+
+    public HelloController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @PostMapping("/call")
+    public String call(@RequestParam String prompt) {
+        return this.chatClient.prompt().messages().user(prompt).call().content();
+    }
+
+}
