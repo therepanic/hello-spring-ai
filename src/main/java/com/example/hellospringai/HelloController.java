c9647a7682ece6a05f6e53b3f1128d9d02585c25
Andrey Litvitski <andrey1010102008@gmail.com>
2025-07-22 21:51:49 +0300

    add receiving and deleting message histories

--- src/main/java/com/example/hellospringai/HelloController.java
+++ src/main/java/com/example/hellospringai/HelloController.java
@@ -2,23 +2,37 @@ package com.example.hellospringai;
 
 import jakarta.servlet.http.HttpSession;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
 
+    private final ChatMemory chatMemory;
     private final ChatClient chatClient;
 
     public HelloController(ChatClient.Builder chatClientBuilder, HttpSession httpSession) {
-        this.chatClient = chatClientBuilder.defaultAdvisors(new PerSessionMessageChatMemoryAdvisor(MessageWindowChatMemory.builder().build(), httpSession)).build();
+        this.chatMemory = MessageWindowChatMemory.builder().build();
+        this.chatClient = chatClientBuilder.defaultAdvisors(new PerSessionMessageChatMemoryAdvisor(this.chatMemory, httpSession)).build();
     }
 
     @PostMapping("/call")
     public String call(@RequestParam String prompt) {
         return this.chatClient.prompt().messages().user(prompt).call().content();
+    }
+
+    @GetMapping("/history")
+    public List<Message> history(HttpSession httpSession) {
+        return this.chatMemory.get(httpSession.getId());
+    }
+
+    @DeleteMapping("/history/clear")
+    public void clearHistory(HttpSession httpSession) {
+        this.chatMemory.clear(httpSession.getId());
     }
 
 }
